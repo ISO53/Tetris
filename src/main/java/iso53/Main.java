4bd768797d2cdf7f12dddc5f4bae5a77a1968740
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-09-04 15:33:03 +0300

    created basic JFrame application main class

--- src/main/java/iso53/Main.java
+++ src/main/java/iso53/Main.java
@@ -1,4 +1,36 @@
 package iso53;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
+
 public class Main {
+    private JPanel jPanelBackPanel;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new);
+    }
+
+    public Main() {
+        JFrame frame = new JFrame("Tetris");
+        frame.setContentPane(jPanelBackPanel);
+        frame.setMinimumSize(new Dimension(400, 700));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        // Set application icon
+        try {
+            frame.setIconImage(ImageIO.read(Objects.requireNonNull(getClass().getResource("/logo.png"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        GamePanel gp = new GamePanel();
+        jPanelBackPanel.add(gp);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
